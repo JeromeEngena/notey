9dab7ba0e5eb7615a40207e51f3f5e3b286bc6b1
Jerome <jembrian7@gmail.com>
2024-02-19 18:45:43 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/kakak/notey/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kakak.notey
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/register_view.dart
@@ -0,0 +1,74 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+                    return Column(
+                  children: [
+                    TextField(
+                      controller: _email,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                          hintText: 'Enter your email here'),
+                    ),
+                    TextField(
+                      controller: _password,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: const InputDecoration(
+                          hintText: 'Enter your password here'),
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
+                        try {
+                          final userCredential = await FirebaseAuth.instance
+                              .createUserWithEmailAndPassword(
+                                  email: email, password: password);
+                          print(userCredential);
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'email-already-in-use') {
+                            print(e.code.toLowerCase().replaceAll('-', ' '));
+                          } else if (e.code == 'weak-password') {
+                            print(e.code.toLowerCase().replaceAll('-', ' '));
+                            print('Password should be at least 6 characters.');
+                          } else if (e.code == 'invalid-email') {
+                            print(e.code.toLowerCase().replaceAll('-', ' '));
+                          }
+                        }
+                      },
+                      child: const Text('Register'),
+                    ),
+                  ],
+                );
+    }
+}
