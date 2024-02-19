26d7781f0098bf81a2985c97b4d5ec3f71059193
Jerome <jembrian7@gmail.com>
2024-02-19 20:26:19 +0300

    linked register view to login view

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notey/firebase_options.dart';
 import 'package:notey/views/login_view.dart';
+import 'package:notey/views/register_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +16,10 @@ void main() {
         useMaterial3: true,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView()
+      },
     ),
   );
 }
@@ -43,7 +48,7 @@ class HomePage extends StatelessWidget {
                 return const LoginView();
 
               default:
-                return const Text('Loading...');
+                return const CircularProgressIndicator();
             }
           }),
     );
