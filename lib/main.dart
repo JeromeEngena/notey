222d52de3570fd8d83b372143aba5a60ce35e981
Jerome <jembrian7@gmail.com>
2024-02-21 14:44:11 +0300

    moved constants to their own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notey/constants/routes.dart';
 import 'package:notey/firebase_options.dart';
 import 'package:notey/views/login_view.dart';
 import 'package:notey/views/register_view.dart';
@@ -19,9 +20,9 @@ void main() async {
       ),
       home: const LoginView(),
       routes: {
-        '/login/': (context) => const LoginView(),
-        '/register/': (context) => const RegisterView(),
-        '/notes/': (context) => const NotesView(),
+        loginRoute: (context) => const LoginView(),
+        registerRoute: (context) => const RegisterView(),
+        notesRoute: (context) => const NotesView(),
       },
     ),
   );
@@ -79,7 +80,7 @@ class _NotesViewState extends State<NotesView> {
                   await FirebaseAuth.instance.signOut();
                   if (context.mounted) {
                     Navigator.of(context).pushNamedAndRemoveUntil(
-                      '/login/',
+                      loginRoute,
                       (_) => false,
                     );
                   }
