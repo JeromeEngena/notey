50fef9b38a0477d986293e2421fa85e92fa891aa
Jerome <jembrian7@gmail.com>
2024-03-05 13:45:55 +0300

    updated existing notes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notey/constants/routes.dart';
 import 'package:notey/services/auth/auth_service.dart';
 import 'package:notey/views/login_view.dart';
-import 'package:notey/views/notes/new_note_view.dart';
+import 'package:notey/views/notes/create_update_note_view.dart';
 import 'package:notey/views/notes/notes_view.dart';
 import 'package:notey/views/register_view.dart';
 import 'package:notey/views/verify_email_view.dart';
@@ -24,7 +24,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute:(context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
